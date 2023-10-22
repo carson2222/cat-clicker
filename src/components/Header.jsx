3c9c749ab72d75a9b6a9e604e135be9fbb58cfc8
carson2222 <olcraftyt@gmail.com>
2023-10-22 22:06:49 +0200

    small update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 import logo from "../images/logo.png";
-import "./_header.scss";
+import classes from "./_header.module.scss";
 
 export function Header({}) {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} alt="Cat Clicker Logo" className="logo__img" />
-        <h1 className="logo__text">Cat Clicker</h1>
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <img src={logo} alt="Cat Clicker Logo" className={classes.logo__img} />
+        <h1 className={classes.logo__text}>Cat Clicker</h1>
       </div>
-      <ul className="nav">
-        <li className="nav__item">
+      <ul className={classes.nav}>
+        <li className={classes.nav__item}>
           <a href="#">Home</a>
         </li>
-        <li className="nav__item">
+        <li className={classes.nav__item}>
           <a href="#">Clicker</a>
         </li>
-        <li className="nav__item">
+        <li className={classes.nav__item}>
           <a href="#">Shop</a>
         </li>
-        <li className="nav__item">
+        <li className={classes.nav__item}>
           <a href="#">Faq</a>
         </li>
-        <li className="nav__item">
+        <li className={classes.nav__item}>
           <a href="#">Info</a>
         </li>
       </ul>
